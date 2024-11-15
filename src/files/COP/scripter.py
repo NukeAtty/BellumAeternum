66c68b136e47d2febdcc0e439b9d5d7d1afc4915
NukeAtty <nukeatty@outlook.com>
2024-11-15 09:18:41 +0800

    cop update

--- src/files/COP/scripter.py
+++ src/files/COP/scripter.py
@@ -11,4 +11,4 @@ class Triggers:
 		self.Persistence = Persistence
 
 	def printTrigger(self):
-		print("ID: " + self.ID + "\nBelonging: " + self.House + "\nLinked Trigger:" + self.LinkedTrigger + "\nTriggerName: " + self.Name + "\nInitially Disabled: " + self.Disabled + "\nEasy mode available: " + self.Easy + "\nNormal mode available: " + self.Normal + "\nHard mode available: " + self.Hard + "\nPersistence: " + self.Persistence)
+		print("********************\nID: " + self.ID + "\n所属方：" + self.House + "\n关联触发：" + self.LinkedTrigger + "\n触发名称：" + self.Name + "\n初始关闭：" + self.Disabled + "\n简单模式：" + self.Easy + "\n普通模式：" + self.Normal + "\n困难模式：" + self.Hard + "\n重复：" + self.Persistence)
