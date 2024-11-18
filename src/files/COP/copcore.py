f3bfca61bf3530bbe2e3260d51f830903a875c1e
NukeAtty <136240197+NukeAtty@users.noreply.github.com>
2024-11-18 18:19:46 +0800

    Update copcore.py

--- src/files/COP/copcore.py
+++ src/files/COP/copcore.py
@@ -10,8 +10,6 @@ Triggers = TriggerChopper(InitialTriggers)
 InitialEvents = EventReader(COPWorkflow)
 Events = EventChopper(InitialEvents)
 
-
-
 open('output.txt', 'w').close()
 for x in range (0, len(Triggers)):
 	TextFileWriter(Triggers[x].getTrigger())
